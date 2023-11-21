cbbaba81271872e4888914b688862da7074c000f
Juan8516 <juan.ind85@gmail.com>
2023-11-21 16:29:00 -0500

    Se carga normalize

--- app/root.jsx
+++ app/root.jsx
@@ -18,6 +18,11 @@ export function links() {
     return [
         {
             rel: 'stylesheet',
+            href: 'https://necolas.github.io/normalize.css/8.0.1/normalize.css'
+        },
+
+        {
+            rel: 'stylesheet',
             href: styles
         },
 
@@ -29,9 +34,9 @@ export function links() {
         {
             rel: 'preconnect',
             href: 'https://fonts.gstatic.com',
-            crossOrigin: true 
+            crossOrigin: 'true' 
         },
-        
+
         {
             rel: 'stylesheet',
             href: 'https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&family=Outfit:wght@400;700;900&family=Roboto+Condensed:ital,wght@0,700;1,400&display=swap'
