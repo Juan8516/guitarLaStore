0593b53fae9da5c7cea893461f4dff4e860e2115
Juan8516 <juan.ind85@gmail.com>
2023-11-22 18:05:23 -0500

    Se crea componente para footer

---
+++ app/components/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='footer'>
+      <div className='contenedor contenido'>
+
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
