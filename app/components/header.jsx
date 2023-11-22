88746e6859d3a75cac0693df3529d4d8fc6c82e5
Juan8516 <juan.ind85@gmail.com>
2023-11-21 23:01:00 -0500

    Se importa useLocation para efecto activado en navegacion

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,31 +1,36 @@
 import React from 'react'
-import { Link } from '@remix-run/react'
+import { Link, useLocation } from '@remix-run/react'
 import logo from '../../public/img/logo.svg'
 
+
 function Header() {
-  return (
-    <header className='header'>
-        <div className='contenedor barra'>
-            <Link src='/'>
-                <img className='logo' src={logo} alt="Imagen logo" />
-            </Link>
-            <nav className='navegacion'>
-                <Link
-                    to='/' 
-                >Inicio</Link>
-                <Link
-                    to='/nosotros'
-                >Nosotros</Link>
-                <Link
-                    to='/tienda'
-                >Tienda</Link>
-                <Link
-                    to='/blog'
-                >Blog</Link>
-            </nav>
-        </div>
-    </header>
-  )
+
+    const location = useLocation()
+
+    return (
+        <header className='header'>
+            <div className='contenedor barra'>
+                <Link src='/'>
+                    <img className='logo' src={logo} alt="Imagen logo" />
+                </Link>
+                <nav className='navegacion'>
+                    <Link
+                        to='/'
+                        className={location.pathname === '/' ? 'active' : ''} 
+                    >Inicio</Link>
+                    <Link
+                        to='/nosotros'
+                    >Nosotros</Link>
+                    <Link
+                        to='/tienda'
+                    >Tienda</Link>
+                    <Link
+                        to='/blog'
+                    >Blog</Link>
+                </nav>
+            </div>
+        </header>
+    )
 }
 
 export default Header
