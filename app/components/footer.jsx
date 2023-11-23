f9eb0b0a287ea1d657f055ed14983403196c823a
Juan8516 <juan.ind85@gmail.com>
2023-11-22 19:59:25 -0500

    Se exporta navegacion y se agrega parrafo de derechos

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Navegacion from './navegacion'
 
 function Footer() {
   return (
     <footer className='footer'>
       <div className='contenedor contenido'>
+        <Navegacion />
 
+        <p className='copyright'>Todos los derechos reservados {new Date().getFullYear()}</p>
       </div>
     </footer>
   )
